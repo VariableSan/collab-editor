e1d436fe7bbb8a667819821812b32a0603451f16
VariableSan <nursultan_nurgaliyev@proton.me>
2025-06-20 10:28:51 +0500

    add diff-lib to frontend

--- apps/app/app/pages/index.vue
+++ apps/app/app/pages/index.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import { simpleDiff } from 'diff-lib'
+
 const isDark = useDark({
   selector: 'body',
   attribute: 'data-theme',
@@ -8,6 +10,11 @@ const isDark = useDark({
 })
 
 const toggleDark = useToggle(isDark)
+
+const oldText = ref('')
+const newText = ref('')
+
+const getDiff = computed(() => simpleDiff(oldText.value, newText.value))
 </script>
 
 <template>
@@ -15,5 +22,14 @@ const toggleDark = useToggle(isDark)
     <button @click="() => toggleDark()">toggle theme</button>
 
     <h1 class="font-bold text-2xl text-(--ui-primary)">Nuxt UI - Starter</h1>
+
+    <div class="flex flex-col gap-2">
+      <input v-model="oldText" class="border" type="text" />
+      <input v-model="newText" class="border" type="text" />
+
+      <div>
+        <h1>Result: {{ getDiff }}</h1>
+      </div>
+    </div>
   </div>
 </template>
